9e06a6d8705a74282f3e6101ae94cca7ac3dd5de
bala <dmbala@gmail.com>
2014-11-24 15:55:22 -0600

    revert grid universe

--- namd_run.sh
+++ namd_run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load namd/2.9
-namd2 ubq_gbis_eq.conf > output.$1.dat
+namd2 ubq_gbis_eq.conf > ubq_gbis_eq.log
+mkdir -p OutFilesFromNAMD
+cp * OutFilesFromNAMD/.
+tar cvzf OutFilesFromNAMD.tar.gz OutFilesFromNAMD
+
 
 
 
